086c30d33703860ebc91b932bd781641f60c1605
SameerGiri69 <sameergiri153000@gmail.com>
2024-11-19 21:15:35 +0545

    api setup

--- src/api.tsx
+++ src/api.tsx
@@ -13,8 +13,14 @@ export const searchCompanies = async (query: string) => {
         `https://financialmodelingprep.com/api/v3/search?query=${query}&limit=10&exchange=NASDAQ&apikey=${process.env.REACT_APP_API_KEY}`
       );
       return data;
-    } catch (error) {
-       console.log(error);
+    } catch (e) {
+      if (e instanceof Error) {
+        console.log("error message: ", e.message);
+        return e.message;
+      } else {
+        console.log("Unexpected error: ", e);
+        return "An unexpected error occurred";
+      }
       }
     
   };
